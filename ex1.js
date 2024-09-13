4b499afd4c1455ed97057e3decac5be8fddbe8db
Igor <igormaymelo@gmail.com>
2024-09-12 21:10:41 -0300

    segundo commit

--- ex1.js
+++ ex1.js
@@ -38,3 +38,164 @@ let idade = prompt('Coloque sua idade');
 const texto = `Olá, meu nome é ${nome} e eu tenho ${idade} anos.`
 console.log(texto);
 
+let ano = prompt('Digite seu ano de nascimento');
+idade = 2024 - ano
+
+
+
+
+
+console.log(`Sua idade é de ${idade} anos`);
+
+
+
+
+
+let numero1 = (10);
+let numero2 = (5);
+let numero3 = (8);
+media = (numero1 + numero2 + numero3) / 3;
+console.log(`A média das suas notas foi de ${media} pontos`)
+
+
+
+
+
+let x1 = 1500
+let x2 = 4600
+let x3 = 2800
+let x4 = 3200
+
+let p1 = 3
+let p2 = 6
+let p3 = 5
+let p4 = 10
+
+const media = p1*x1 + p2*x2 + p3*x3 + p4*x4 / p1+p2+p3+p4;
+
+console.log(`A sua média ponderada foi de ${media}`)
+
+
+
+
+
+
+
+let metros = prompt('Digite quantos metros quadrados de tinta você quer')
+let litros = metros * 3
+let lata = litros * 18 / 3
+ 
+console.log(`${lata} latas de tinta vão custar reais`)  
+
+
+
+
+
+let periodo = prompt('Digite seu turno: Noturno, Vespertino ou Matutino')
+
+if (periodo == 'Matutino') {
+    console.log('Bom Dia!')
+}
+else if (periodo == 'Vespertino'){
+    console.log('Boa Tarde!')
+}
+else if (periodo == 'Noturno'){
+    console.log('Boa Noite!')
+}
+else {
+    console.log('Valor Inválido, recarregue a página e escreva novamente')
+}
+
+
+
+
+
+
+let idade = prompt('Digite sua idade')
+if (idade >= 18) {
+    console.log('Você é maior de idade')
+}
+else if (idade < 18) {
+console.log('Você é menor de idade')
+}
+else {
+    console.log('Valor invalido')
+}
+
+
+
+
+
+let ano = prompt('digite um ano')
+
+if (ano % 4 === 0 && (ano % 100 !== 0 || ano % 400 === 0)) {
+    console.log('O ano é bissexto')
+}
+else {
+    console.log('O ano não é bissexto')
+}
+
+
+
+
+
+for (let i = 0; i<=100 ; i++) {
+    console.log(i)
+}
+
+
+
+
+
+
+let soma = 0;
+for (let i = 0; i<= 100; i++){
+    soma += i;
+    console.log(soma)
+}
+
+
+
+
+
+function fatorial(n) {
+    let resultado = 1;
+    for (let i = 2; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+console.log(fatorial(100));
+
+
+
+
+
+
+function refazer(){
+let nome = prompt('Coloque seu nome')
+let senha = prompt('Coloque sua senha')
+
+while (senha === nome){
+    console.log('A senha não pode ser igual ao nome')
+    
+    let nome = prompt('Coloque seu nome')
+    let senha = prompt('Coloque sua senha')
+break
+}
+
+console.log('Entrou com sucesso')
+}
+refazer();
+
+
+
+
+
+
+for (let numero = 1; numero <= 50; numero++) {
+    if (numero % 2 !== 0) {
+        console.log(numero);
+    }
+}
